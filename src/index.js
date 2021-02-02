2d4e8bc7a8926c739e5ebb2e70b2d02684d01a6f
Michael Fuget <michaelfuget@gmail.com>
2021-02-02 08:49:02 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 console.log('%c HI', 'color: firebrick')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-
+let imgUrlDa = 0;
 fetch(imgUrl)
   .then(resp => resp.json())
   .then(json => imgUrlData);
