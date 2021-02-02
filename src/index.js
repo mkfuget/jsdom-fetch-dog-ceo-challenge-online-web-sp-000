40d20089b29bb2805ca2617d15c40ec4dea0f483
Michael Fuget <michaelfuget@gmail.com>
2021-02-02 09:14:46 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -13,12 +13,14 @@ function addImgJson() {
 }
 function renderImgs(json) {
   const main = document.querySelector('body')
+  const ul = document.createElement('ul')
   for(let i=0; i<json.message.length; i++)
   {
-    const img = document.createElement('img')
-    img.src = json.message[i]
-    main.appendChild(img)
+    const li = document.createElement('li')
+    li.innerHTML = json.message[i]
+    ul.appendChild(li)
   }
+  main.appendChild(ul)
 }
 function addbreedJson() {
   const breedUrl = 'https://dog.ceo/api/breeds/list/all'
