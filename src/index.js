227d9dcc2d17bcccdc9281eb0ce69d252918c7bd
Michael Fuget <michaelfuget@gmail.com>
2021-02-02 08:55:38 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 console.log('%c HI', 'color: firebrick')
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 let imgUrlDa = 0;
-fetch(imgUrl)
-  .then(resp => resp.json())
+function getImgJson() {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(json => renderImgs(json))
+  }
 
 console.log("test")
 
 console.log(resp)
+function renderImgs(json) {
+
+}
