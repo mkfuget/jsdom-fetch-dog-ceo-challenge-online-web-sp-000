4a2c9a69ee4dc1d13d3fbabe4b1490224662d3a8
Michael Fuget <michaelfuget@gmail.com>
2021-02-03 03:37:53 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ function renderbreeds(json) {
     const ul = document.getElementById('dog-breeds')
     alert("test")
     for(let child=ul.firstChild; child!==null; child=child.nextSibling) {
-        if(child.innerHTML.charAt(0) != this.value)
+        if(child.innerHTML.charAt(0) != event.srcElement.value)
         {
           child.style = "none"
         }
