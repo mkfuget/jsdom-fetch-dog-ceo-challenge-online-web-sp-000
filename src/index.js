52d0446e64a2652a71538ea92a32ee2e9f2c26d9
Michael Fuget <michaelfuget@gmail.com>
2021-02-03 03:40:18 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ function renderbreeds(json) {
   const selectBox = document.getElementById('breed-dropdown');
   selectBox.addEventListener('change', (event) => {
     const ul = document.getElementById('dog-breeds')
-    alert("test")
+    event.target
     for(let child=ul.firstChild; child!==null; child=child.nextSibling) {
         if(child.innerHTML.charAt(0) != event.target.value)
         {
